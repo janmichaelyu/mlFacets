09c5a6df47ca01737352ef770cf961347fba1215
Dave Cassel <dmcassel@gmail.com>
2014-04-18 17:25:07 -0400

    Updating to use a string template instead of templateUrl

--- dist/ml-facets.js
+++ dist/ml-facets.js
@@ -2,7 +2,23 @@
 
   'use strict';
 
-  angular.module('mlFacets')
+  var template =
+    '<div class="facet-list">\n' +
+      '<div class="chiclets">\n' +
+        '<div class="btn btn-primary" ng-repeat="chiclet in selected">\n' +
+          '<span>{{chiclet.facet}}: {{chiclet.value}}</span>\n' +
+          '<span class="glyphicon glyphicon-remove-circle icon-white" ng-click="clear(chiclet)"></span>\n' +
+        '</div>\n' +
+      '</div>\n' +
+      '<div class="facet" ng-repeat="(name, facet) in facets">\n' +
+        '<h3>{{ name }}</h3>\n' +
+        '<div ng-repeat="value in facet.facetValues">\n' +
+          '<a ng-click="select({facet: name, value: value.name})">{{value.name}} ({{value.count}})</a>\n' +
+        '</div>\n' +
+      '</div>\n' +
+    '</div> ';
+
+  angular.module('mlFacets', [])
     .directive('mlFacets', [function () {
       return {
         restrict: 'AE',
@@ -13,7 +29,7 @@
           select: '&select',
           clear: '&clear'
         },
-        templateUrl: 'ml-facets.html',
+        template: template,
         link: function() {
         }
       };
